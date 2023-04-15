7d60ab1f33601af1a0f28273e1a9c44a9f4bb628
Arakakija <48024442+Arakakija@users.noreply.github.com>
2023-04-15 08:13:50 -0300

    Card Face And Back

--- Deckbuilder/Assets/Scripts/Card/Card.cs
+++ Deckbuilder/Assets/Scripts/Card/Card.cs
@@ -1,17 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Card : MonoBehaviour,IDrag
+public class Card : MonoBehaviour,IDrag, IPointerEnterHandler,IPointerExitHandler
 {
-    
     public void OnStartDrag()
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public void OnEndDrag()
     {
-        throw new System.NotImplementedException();
+        
+    }
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Entro Mouse");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Salio Mouse");
     }
 }
